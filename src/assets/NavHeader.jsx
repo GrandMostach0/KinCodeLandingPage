b3d601511ed3e6d9f460f00ce5131f1caa021ae1
GrandMostach0 <kreedlegend0@gmail.com>
2025-06-23 12:10:22 -0600

    nav skeleton

---
+++ src/assets/NavHeader.jsx
@@ -0,0 +1,19 @@
+import logoKinCode from './logoKinCode.svg';
+function NavHeader() {
+    return (
+        <div className='flex items-center justify-between py-2 px-10'>
+
+            <div className='flex-1/2'>
+                <img className='w-[10%]' src={logoKinCode} alt="Logo" />
+            </div>
+
+            <nav>
+                <a className='navItem' href="">Servicios</a>
+                <a className='navItem' href="">Contacto</a>
+                <a className='navItem' href="">Comenzar Proyecto</a>
+            </nav>
+        </div>
+    )
+}
+
+export default NavHeader;
